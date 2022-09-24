70bc2192ef1a3f7618cd8841dedb4fa82aa7e626
Bluexhxx <Xhcode200@tom.com>
2022-09-24 23:34:00 +0800

    feat:初始化项目完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,25 @@
-export default  {
+import { loginAPI } from '@/api/login'
+
+export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    token: null
+  },
+  mutations: {
+    SET_TOKEN(state, playload) {
+      state.token = playload
+    }
+  },
+  actions: {
+    async loginAction({ commit }, loginData) {
+      // 请求
+      try {
+        const { data: { data }} = await loginAPI(loginData)
+        // 提交mutions
+        commit('SET_TOKEN', data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
 }
