542339970f468b4f69a44c2e1bcad4752f4943af
Bluexhxx <Xhcode200@tom.com>
2022-09-26 19:45:46 +0800

    feat:下载持久化插件

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,8 @@ import getters from './getters'
 import app from './modules/app'
 import settings from './modules/settings'
 import user from './modules/user'
-
+// yarn add vuex-persistedstate@3.2.1
+import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
@@ -13,7 +14,19 @@ const store = new Vuex.Store({
     settings,
     user
   },
-  getters
+  getters,
+  plugins: [
+    createPersistedState({
+      key: 'LKD_TOKEN',
+      // path: ['user.token']
+      reducer(state) {
+        return {
+          // reducer 层级要和vuex一样
+          user: { token: state.user.token }
+        }
+      }
+    })
+  ]
 })
 
 export default store
