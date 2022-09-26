8720177bd4ce03e2d057303ee11890cdb5e78a86
Bluexhxx <Xhcode200@tom.com>
2022-09-26 20:11:12 +0800

    完成主页中根据有无token，进行页面访问的处理

--- src/permission.js
+++ src/permission.js
@@ -1,1 +1,24 @@
 // 权限允许
+import store from './store'
+import router from './router'
+
+const whiteList = ['/login', '/404']
+// 定义路由前置守卫
+router.beforeEach((to, from, next) => {
+//   console.log(store.getters.token)
+  if (store.getters.token) {
+    // 已登录
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    // 未登录
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
